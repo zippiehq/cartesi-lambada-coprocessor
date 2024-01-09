925600913ea6c6c4418fb39a4d00e7723f080100
not4x217 <6069441+not4x217@users.noreply.github.com>
2024-01-09 14:15:27 +0800

    task carries byte data to echo

--- contracts/src/IIncredibleSquaringTaskManager.sol
+++ contracts/src/IIncredibleSquaringTaskManager.sol
@@ -26,7 +26,7 @@ interface IIncredibleSquaringTaskManager {
 
     // STRUCTS
     struct Task {
-        uint256 numberToBeSquared;
+        bytes input;
         uint32 taskCreatedBlock;
         // task submitter decides on the criteria for a task to be completed
         // note that this does not mean the task was "correctly" answered (i.e. the number was squared correctly)
@@ -44,7 +44,8 @@ interface IIncredibleSquaringTaskManager {
         // Can be obtained by the operator from the event NewTaskCreated.
         uint32 referenceTaskIndex;
         // This is just the response that the operator has to compute by itself.
-        uint256 numberSquared;
+        bytes resultCID;
+        bytes32 outputHash;
     }
 
     // Extra information related to taskResponse, which is filled inside the contract.
@@ -58,7 +59,7 @@ interface IIncredibleSquaringTaskManager {
     // FUNCTIONS
     // NOTE: this function creates new task.
     function createNewTask(
-        uint256 numberToBeSquared,
+        bytes calldata input,
         uint32 quorumThresholdPercentage,
         bytes calldata quorumNumbers
     ) external;
--- contracts/src/IncredibleSquaringTaskManager.sol
+++ contracts/src/IncredibleSquaringTaskManager.sol
@@ -81,13 +81,13 @@ contract IncredibleSquaringTaskManager is
     /* FUNCTIONS */
     // NOTE: this function creates new task, assigns it a taskId
     function createNewTask(
-        uint256 numberToBeSquared,
+        bytes calldata input,
         uint32 quorumThresholdPercentage,
         bytes calldata quorumNumbers
     ) external onlyTaskGenerator {
         // create a new task struct
         Task memory newTask;
-        newTask.numberToBeSquared = numberToBeSquared;
+        newTask.input = input;
         newTask.taskCreatedBlock = uint32(block.number);
         newTask.quorumThresholdPercentage = quorumThresholdPercentage;
         newTask.quorumNumbers = quorumNumbers;
@@ -180,7 +180,7 @@ contract IncredibleSquaringTaskManager is
         BN254.G1Point[] memory pubkeysOfNonSigningOperators
     ) external {
         uint32 referenceTaskIndex = taskResponse.referenceTaskIndex;
-        uint256 numberToBeSquared = task.numberToBeSquared;
+        bytes memory input = task.input;
         // some logical checks
         require(
             allTaskResponses[referenceTaskIndex] != bytes32(0),
@@ -204,9 +204,9 @@ contract IncredibleSquaringTaskManager is
         );
 
         // logic for checking whether challenge is valid or not
-        uint256 actualSquaredOutput = numberToBeSquared * numberToBeSquared;
-        bool isResponseCorrect = (actualSquaredOutput ==
-            taskResponse.numberSquared);
+        bytes32 actualOutput = keccak256(input);
+        bool isResponseCorrect = (actualOutput ==
+            taskResponse.outputHash);
 
         // if response was correct, no slashing happens so we return
         if (isResponseCorrect == true) {
