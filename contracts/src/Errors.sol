d58d5fb102842895c13093d76dcb74a6cdc3cdbe
not4x217 <6069441+not4x217@users.noreply.github.com>
2024-08-05 19:22:07 +0800

    add operator whitelist, remove unused freezerOperator method

---
+++ contracts/src/Errors.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+error NotOperatorWhitelister();
+error OperatorWhitelistAlreadyEnabled();
+error OperatorWhitelistAlreadyDisabled();
+error InvalidOpeatroAddress();
+error OperatorAlreadyInWhitelist();
+error OperatorNotInWhitelist();
