ec82d34671a107739d90388a5fedf470d8ea624f
nyakiomaina <carolinenyakio21@gmail.com>
2024-09-30 20:56:39 +0300

    listen to events

--- coprocessor-explorer/src/components/eventListener.js
+++ coprocessor-explorer/src/components/eventListener.js
@@ -16,6 +16,7 @@ import { getProvider, getContract } from '../config';
 
 const EventListener = () => {
     const [taskResponses, setTaskResponses] = useState([]);
+    const [taskBatches, setTaskBatches] = useState([]); // For TaskBatchRegistered events
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
 
@@ -25,15 +26,14 @@ const EventListener = () => {
 
         const contract = getContract(provider);
 
-        // Listen to TaskResponded events
-        contract.on('TaskResponded', (batchIndex, programId, inputHash, resultCID, outputHash) => {
-            // Decode CIDs
+        // TaskResponded events
+        contract.on('TaskResponded', (task, response) => {
             let decodedProgramId;
             let decodedResultCID;
 
             try {
-                decodedProgramId = CID.decode(Buffer.from(programId.slice(2), 'hex')).toString();
-                decodedResultCID = CID.decode(Buffer.from(resultCID.slice(2), 'hex')).toString();
+                decodedProgramId = CID.decode(Buffer.from(task.programId.slice(2), 'hex')).toString();
+                decodedResultCID = CID.decode(Buffer.from(response.resultCID.slice(2), 'hex')).toString();
             } catch (error) {
                 console.error('Error decoding CID:', error);
                 decodedProgramId = 'Invalid CID';
@@ -41,19 +41,33 @@ const EventListener = () => {
             }
 
             const newResponse = {
-                batchIndex: batchIndex.toNumber(),
+                batchIndex: task.batchIndex.toNumber(),
                 programId: decodedProgramId,
-                inputHash: ethers.utils.hexlify(inputHash),
+                inputHash: ethers.utils.hexlify(task.inputHash),
                 resultCID: decodedResultCID,
-                outputHash: ethers.utils.hexlify(outputHash),
+                outputHash: ethers.utils.hexlify(response.outputHash),
             };
             setTaskResponses((prev) => [newResponse, ...prev]);
             console.log('TaskResponded:', newResponse);
         });
 
-        // Cleanup on component unmount
+        // TaskBatchRegistered events
+        contract.on('TaskBatchRegistered', (batch) => {
+            const newBatch = {
+                index: batch.index.toNumber(),
+                blockNumber: batch.blockNumber.toNumber(),
+                merkeRoot: ethers.utils.hexlify(batch.merkeRoot),
+                quorumNumbers: ethers.utils.hexlify(batch.quorumNumbers),
+                quorumThresholdPercentage: batch.quorumThresholdPercentage.toNumber(),
+            };
+            setTaskBatches((prev) => [newBatch, ...prev]);
+            console.log('TaskBatchRegistered:', newBatch);
+        });
+
+        // Cleanup
         return () => {
             contract.removeAllListeners('TaskResponded');
+            contract.removeAllListeners('TaskBatchRegistered');
         };
     }, []);
 
@@ -99,6 +113,45 @@ const EventListener = () => {
                 <TablePagination
                     component="div"
                     count={taskResponses.length}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    rowsPerPage={rowsPerPage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                    rowsPerPageOptions={[5, 10, 25]}
+                />
+            </TableContainer>
+
+            <Typography variant="h5" gutterBottom style={{ marginTop: '20px' }}>
+                Task Batch Registered Events
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Batch Index</TableCell>
+                            <TableCell>Block Number</TableCell>
+                            <TableCell>Merke Root</TableCell>
+                            <TableCell>Quorum Numbers</TableCell>
+                            <TableCell>Quorum Threshold Percentage</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {taskBatches
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((batch, idx) => (
+                                <TableRow key={idx}>
+                                    <TableCell>{batch.index}</TableCell>
+                                    <TableCell>{batch.blockNumber}</TableCell>
+                                    <TableCell>{batch.merkeRoot}</TableCell>
+                                    <TableCell>{batch.quorumNumbers}</TableCell>
+                                    <TableCell>{batch.quorumThresholdPercentage}</TableCell>
+                                </TableRow>
+                            ))}
+                    </TableBody>
+                </Table>
+                <TablePagination
+                    component="div"
+                    count={taskBatches.length}
                     page={page}
                     onPageChange={handleChangePage}
                     rowsPerPage={rowsPerPage}
